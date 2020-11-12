b884cdea9bbd43c5f26023e4ab19b97559843cb3
iris <huangiris1@gmail.com>
2020-11-11 16:54:21 -0800

    updated tables

--- setup.php
+++ setup.php
@@ -36,7 +36,133 @@ runthis('CREATE TABLE Owner_Has_Dog (
                  ON DELETE CASCADE
 )
 ');
+runthis('CREATE TABLE Dog_Has_Profile_Page (
+  since           DATE,
+  introduction    TINYTEXT,
+  profile_pics    BLOB,
+  profile_id      INT           PRIMARY KEY AUTO_INCREMENT,
+  d_id            INT           NOT NULL,
+  FOREIGN KEY(d_id) REFERENCES Owner_Has_Dog(d_id)
+      ON DELETE CASCADE
+)
+');
+
+runthis('CREATE TABLE Dog_Has_Personal_Note (
+  d_id            INT         NOT NULL,
+  since           DATE        NOT NULL,
+  sender_id       INT         NOT NULL,
+  note_id         INT         PRIMARY KEY AUTO_INCREMENT,
+  FOREIGN KEY(d_id, sender_id) REFERENCES Owner_Has_Dog(d_id)
+      ON DELETE CASCADE
+)
+');
+
+runthis('CREATE TABLE Matches (
+  d1_id       INT,
+  d2_id       INT,
+  start_date  DATE,
+  PRIMARY KEY (d1_id, d2_id),
+  FOREIGN KEY (d1_id, d2_id) REFERENCES Owner_Has_Dog(d_id)
+      ON DELETE CASCADE
+)
+');
+
+runthis('CREATE TABLE Profile_Page_Contains_Post (
+  post_id             INT       PRIMARY KEY,
+  image               BLOB,
+  text                TINYTEXT,
+  num_likes           INT,
+  profile_id          INT       NOT NULL,
+  FOREIGN KEY(profile_id) REFERENCES Dog_Has_Profile_Page(profile_id)
+      ON DELETE CASCADE
+)
+');
+
+runthis('CREATE TABLE Post_Contains_Comment (
+  time_stamp        TIMESTAMP,
+  poster_id         INT,
+  text              TINYTEXT      NOT NULL,
+  num_likes         INT,
+  since             DATE,
+  post_id           INT           NOT NULL,
+  PRIMARY KEY(time_stamp, poster_id, post_id),
+  FOREIGN KEY(post_id) REFERENCES Profile_Page_Contains_Post(post_id)
+      ON DELETE CASCADE,
+  FOREIGN KEY(poster_id) REFERENCES Owner_Has_Dog(d_id) 
+      ON DELETE CASCADE
+)
+');
+
+runthis('CREATE TABLE Owner_Manages_Group (
+  since                DATE,
+  group_id             INT          PRIMARY KEY AUTO_INCREMENT,
+  group_name          VARCHAR(30)   NOT NULL,
+  group_description   TINYTEXT,
+  num_members         INT,
+  user_id             INT           NOT NULL,
+  group_type_id       INT           NOT NULL,
+  FOREIGN KEY (group_type_id) REFERENCES Group_Types(group_type_id)
+      ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES Owner(user_id)
+      ON DELETE CASCADE
+)
+');
+
+runthis('CREATE TABLE Group_Types (
+  group_type_id       INT             PRIMARY KEY AUTO_INCREMENT,
+  group_type          VARCHAR(20)     NOT NULL
+)
+');
+
+runthis('CREATE TABLE Premium_User (
+  user_id         INT       PRIMARY KEY AUTO_INCREMENTS,
+  payment_info    INT       NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES Owner(user_id)
+      ON DELETE CASCADE
+)
+');
+
+runthis('CREATE TABLE Dog_Joins_Group (
+  d_id            INT       PRIMARY KEY AUTO_INCREMENTS,
+  group_id        INT       PRIMARY KEY AUTO_INCREMENTS,
+  since           DATE,
+  FOREIGN KEY(group_id) REFERENCES Owner_Manages_Group(group_id)
+      ON DELETE CASCADE,
+  FOREIGN KEY (d_id) REFERENCES Owner_Has_Dog(d_id)
+      ON DELETE CASCACADE
+)
+');
+
+runthis('CREATE TABLE Dog_Has_Highlights (
+  highlight_id        INT           PRIMARY KEY AUTO_INCREMENT,
+  image               BLOB          NOT NULL,
+  start_time          TIMESTAMP     NOT NULL,
+  numViews            INT,
+  d_id                INT           NOT NULL,
+  FOREIGN KEY(d_id) REFERENCES Owner_Has_Dog(d_id)
+      ON DELETE CASCADE
+)
+');
+
+runthis('CREATE TABLE Premium_Profile_Page (
+  profile_id            INT         PRIMARY KEY AUTO_INCREMENTS,
+  premium_swag_id       INT,
+  FOREIGN KEY(profile_id) REFERENCES Dog_Has_Profile_Page(profile_id)
+      ON DELETE CASCADE,
+  FOREIGN KEY(premium_swag_id) REFERENCES Premium_Swags(premium_swag_id)
+      ON DELETE CASCADE
+)
+');
+
+runthis('CREATE TABLE Premium_Swags (
+  premium_swag_id       INT       PRIMARY KEY AUTO_INCREMENT,
+  premium_swag_type     VARCHAR(20),
+  premium_swag_content  BLOB
+)
+');
+
 /****INSERT MORE BELOW******/
+
     
 /****DO NOT INSERT AFTER THIS******/    
 echo 'tables created';
